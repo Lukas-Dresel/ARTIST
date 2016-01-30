fe34d6557682bedd2172de6e48d61cee7ade3866
Lukas Dresel <Lukas-Dresel@users.noreply.github.com>
2016-01-31 00:40:08 +0100

    Added skeleton for hooking functions. This design is then supposed to hook functions by simply installing the restoration trappoint at the return address

---
+++ Android_Projects/NdkTest/app/src/main/jni/hooking/function_hook.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Lukas on 1/31/2016.
+//
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "function_hook.h"
+
+#ifdef __cplusplus
+}
+#endif
+
---
+++ Android_Projects/NdkTest/app/src/main/jni/hooking/function_hook.h
@@ -0,0 +1,10 @@
+//
+// Created by Lukas on 1/31/2016.
+//
+
+#ifndef NDKTEST_FUNCTION_HOOK_H
+#define NDKTEST_FUNCTION_HOOK_H
+
+
+
+#endif //NDKTEST_FUNCTION_HOOK_H
